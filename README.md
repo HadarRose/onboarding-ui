53233407c1bb1926a67e15750c4d1156dfd753eb
HadarRose <rosenberg07@gmail.com>
2020-08-13 11:51:20 -0500

    Stylistic changes
    - Added more comments
    - Removed some unnecessary features
    - Added better declarations to index.html
    - Added README.md file

---
+++ README.md
@@ -0,0 +1,8 @@
+# onboarding-ui
+### Dependencies and Requirements: 
+* Node.js: v12.18.3
+
+### Instructions
+* Run: ```node src/js/main.js``` from root directory
+* Endpoint: 
+    * http://localhost:9000
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'); // docs: https://nodejs.org/api/http.html
+const fs = require('fs'); // docs: https://nodejs.org/docs/v0.3.1/api/fs.html
 
-const port = 9000;
+const port = 9000; // port to which this server will be listening 
 
 let handleRequest = ((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./src/index.html',null, function (err, html){
+    // read file from specified path, and process it with err being the possible error thrown and html being the content read
+    fs.readFile('./src/index.html', function (err, html){
         if (err){
-            res.writeHead(404);
-            res.write(":(");
+            res.writeHead(404); // change status to error in case of an error. Can write my own error here, if I'd like. 
         } else {
             res.write(html);
         }
@@ -17,5 +17,3 @@ let handleRequest = ((req, res) => {
 });
 
 http.createServer(handleRequest).listen(port);
-
-// tutorials: https://vegibit.com/render-html-in-node-js/
