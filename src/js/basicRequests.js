829898a680c544861d96157b1e6bf2f922526c09
HadarRose <rosenberg07@gmail.com>
2020-08-17 13:08:26 -0500

    Added bad status handling

--- src/js/basicRequests.js
+++ src/js/basicRequests.js
@@ -2,8 +2,13 @@ function getTimeline() {
     console.info('Requesting timeline from backend.');
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){ // more on readystates: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-        if(this.readyState == XMLHttpRequest.DONE && this.status == 200){ // waits for request to be done and successful (status 200)
-            document.getElementById("timeline-para").innerHTML = this.responseText;
+        if(this.readyState == XMLHttpRequest.DONE){ // waits for request to be done and successful (status 200)
+            if(this.status == 200){
+                document.getElementById("timeline-para").innerHTML = this.responseText;
+            } else{
+                document.getElementById("timeline-para").innerHTML = "Something went wrong.";
+                console.error("Bad response: " + this.status);
+            }
         } 
     };
     xhttp.onerror = function(){
