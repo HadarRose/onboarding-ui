c7f8982e3c599992b95e29ed263b13d707601c9c
HadarRose <rosenberg07@gmail.com>
2020-08-17 09:52:53 -0500

    Minor changes
    - used variable instead of 4 for DONE
    - changed body to head at index.html

--- src/js/basicRequests.js
+++ src/js/basicRequests.js
@@ -2,7 +2,7 @@ function getTimeline() {
     console.info('Requesting timeline from backend.');
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){ // more on readystates: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
-        if(this.readyState == 4 && this.status == 200){ // waits for request to be done (readyState 4) and successful (status 200)
+        if(this.readyState == XMLHttpRequest.DONE && this.status == 200){ // waits for request to be done and successful (status 200)
             document.getElementById("timeline-para").innerHTML = this.responseText;
         } 
     };
