d44972b9bfbcae2324506ee3ffdb7e850cf9da05
HadarRose <rosenberg07@gmail.com>
2020-09-18 13:40:54 -0500

    changed timeline display to tabs and added post tweet feature

---
+++ src/js/components/PostTweet.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { postTweet } from '../services/timelineService';
+
+export default class PostTweet extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            message: '',
+            error: null,
+            isLoaded: false,
+            charCount: 0
+        };
+    }
+
+    handleInputChange(event){
+        this.setState({
+            message: event.target.value,
+            charCount: event.target.value.length
+        });
+    }
+
+    postTweet(){
+        this.setState({
+            error: null,
+            isLoaded: false
+        });
+        postTweet(this.state.message).then(
+            (response) => {
+                this.setState({
+                    isLoaded: true
+                });
+            }, 
+            (error) => {
+                this.setState({
+                    error: error,
+                    isLoaded: true
+                });
+            }
+        );
+
+    }
+
+    renderReport(){
+        if(this.state.error && this.state.isLoaded){
+            return (<span className='report error'>There was an issue posting your tweet.</span>);
+        } else if(this.state.isLoaded){
+            return(<span className='report success'>Successfully posted your tweet.</span>);
+        }
+    }
+
+    render(){
+        return (
+            <div className='post-tweet-container'>
+                <div className='post-text-area'>
+                    <textarea
+                        className='post-input'
+                        value={this.state.message}
+                        onChange={(event) => this.handleInputChange(event)}
+                        maxLength="280"
+                    />
+                    <div className='char-count' data-count={this.state.charCount}>
+                        {this.state.charCount}/280
+                    </div>
+                </div>
+                <div className='post-controls'>
+                    {
+                        this.renderReport()
+                    }
+                    <button
+                        className='post-button'
+                        disabled={!this.state.message?.length || this.state.message == ''}
+                        onClick={() => this.postTweet()}
+                    >Post</button>
+                </div>
+            </div>
+        );
+    }
+}
--- src/js/components/timelinePage.js
+++ src/js/components/timelinePage.js
@@ -1,15 +1,63 @@
 import React from 'react';
 import Timeline from './timeline';
+import PostTweet from './PostTweet';
 
 export default class TimelinePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            activeTab: 'general'
+        }
+    }
+
+    get TABS(){
+        return {
+            general: {
+                id: 'general-timeline',
+                title: 'General Timeline',
+                render: () => {return <Timeline key='default'/>}
+            },
+            self: {
+                id: 'self-timeline',
+                title: 'User Timeline',
+                render: () => {return <Timeline type='self' key='self'/>;}
+            },
+            post: {
+                id: 'post-tweet',
+                title: 'Post Tweet',
+                render: () => {return <PostTweet/>}
+            }
+        };
+    }
+
+    clickTab(key){
+        this.setState({
+            activeTab: key
+        });
+    }
+
+    renderButton(key, tab){
+        return (<button key={tab.id}
+                className='tab'
+                data-selected={this.state.activeTab == key}
+                onClick={() => this.clickTab(key)}
+                >{tab.title}</button>);
+    }
+
     render(){
         return(
             <div className="main-container">
                 <div className="title-div">Lab for Hadar</div>
-                <div className="timelines">
-                    <Timeline/>
-                    <Timeline type="self" title="Your Timeline"/>
+                <div className="tabs-container">
+                    {
+                        Object.entries(this.TABS).map(([key, tab]) =>( 
+                            this.renderButton(key, tab)
+                        ))
+                    }
                 </div>
+                {
+                    this.TABS[this.state.activeTab].render()
+                }
             </div>
         );
     }
